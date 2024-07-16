321bc58d8b1dd002b8044b3cc933bb22552d8227
real-zephex <zephex-1@outlook.com>
2024-07-16 23:35:25 +0530

    feat: trying out google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="dracula">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-7424574858811768" />
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-WSQFR03XW0"
