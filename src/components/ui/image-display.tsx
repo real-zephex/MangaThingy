c58f624e2cd95cb623656530dc0a6312b9a48620
real-zephex <zephex-1@outlook.com>
2024-07-16 19:41:23 +0530

    welp, reverted from the internal proxy system as it was causing some really bad issues for the android users. switched to a cloudflare workers to proxy the images

--- src/components/ui/image-display.tsx
+++ src/components/ui/image-display.tsx
@@ -4,11 +4,21 @@ import Image from "next/image";
 import { Key } from "react";
 import { ImageUrls } from "../data/types";
 
+const proxy = "https://sup-proxy.zephex0-f6c.workers.dev/api-content?url=";
+
 const ImageDisplay = async (data: ImageUrls, provider: string) => {
   var mangapillCondition: boolean;
+  var referer: string = "";
+  var refererCollection = {
+    mangapill: "https://mangapill.com/",
+    manganato: "https://chapmanganato.to/",
+  };
+
   if (provider == "mangapill" || provider == "manganato") {
     mangapillCondition = true;
+    referer = refererCollection[provider];
   }
+
   return (
     <div className="mt-1 flex flex-col items-center w-full">
       {data &&
@@ -18,11 +28,7 @@ const ImageDisplay = async (data: ImageUrls, provider: string) => {
             width={1080}
             height={4000}
             key={index}
-            src={
-              mangapillCondition
-                ? `https://manga-scrapers.onrender.com/${provider}/images/${item}`
-                : item
-            }
+            src={`${proxy}${item}&headers="${referer}"`}
             alt="Manga Page"
             className="w-full h-full"
             priority
