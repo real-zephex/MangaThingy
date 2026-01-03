f64dd21274ffc605c818d040e4436447e91bda7e
real-zephex <zephex-1@outlook.com>
2026-01-04 00:13:40 +0530

    fix: update fetch URL to use base URL function

--- lib/services/manga.service.ts
+++ lib/services/manga.service.ts
@@ -1,16 +1,9 @@
 "use server";
 
-const getBaseUrl = () => {
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
-  }
-  return "http://localhost:3000";
-};
-
 export const fetchWrapper = async (endpoint: string) => {
   try {
-    const baseUrl = getBaseUrl();
-    const response = await fetch(`${baseUrl}/api${endpoint}`, {
+    // const baseUrl = getBaseUrl();
+    const response = await fetch(`https://manga-thingy.vercel.app/api${endpoint}`, {
       next: { revalidate: 3600 }, // Revalidate for 1 hour
     });
     if (!response.ok) {
