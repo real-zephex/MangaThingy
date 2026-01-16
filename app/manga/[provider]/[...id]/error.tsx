bcaf38a418d765c042ed4ed4899620148119173b
real-zephex <zephex-1@outlook.com>
2026-01-16 19:37:35 +0530

    added error.tsx

---
+++ app/manga/[provider]/[...id]/error.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { AlertCircleIcon } from "lucide-react";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="w-full h-full flex flex-col items-center gap-2">
+      <div className="flex flex-row items-center gap-2">
+        <AlertCircleIcon />
+        <h2>Something went wrong!</h2>
+      </div>
+      <Button onClick={() => reset()}>Try again</Button>
+    </div>
+  );
+}
