8f0c47032bcc46ec2db0d3b7b9f7f5512e3a2494
real-zephex@popos <zephex-1@outlook.com>
2026-03-02 20:26:48 +0530

    opted out of next js image optimization

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* Image optimization */
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
